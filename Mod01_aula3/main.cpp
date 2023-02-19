f6a7e4aba42622f16d30abb9b7a22c1562d72c8e
gabrielrg <gabriel.rguerreiro@gmail.com>
2023-02-18 22:56:06 -0800

    classe simples

--- Mod01_aula3/main.cpp
+++ Mod01_aula3/main.cpp
@@ -5,9 +5,34 @@ using std::cin;
 using std::endl;
 
 #include <string> //classe de string no padrao c++
+using std::string;
+using std::getline;
+
+//==================================================
+// Desenvolvimento da classe
+class SalesScore
+{
+    public: //public é um especificador de acesso
+
+        void bootSystem(string storeTitle)
+        {
+            cout << "Score de vendas: \n" << storeTitle << endl;
+        } //end bootSystem
+};
+
+//==================================================
+// Desenvolvimento da funcao principal
 
 int main()
 {
-    cout << "teste";
+    string storeTitle; // variavel local string para nome da loja
+    SalesScore mySales; // objeto mySales
+
+    cout << "Insira o nome da loja: ";
+    getline(cin, storeTitle); //ler o nome da loja com espacos em branco
+    cout << endl;
+
+    mySales.bootSystem(storeTitle);
     return 0;
 }
+// Final do programa
