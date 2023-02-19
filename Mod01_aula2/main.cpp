a82f4c5a856b5fc01bf5ab7fa6d4471ef020b92e
gabrielrg <gabriel.rguerreiro@gmail.com>
2023-02-18 22:10:08 -0800

    casting

--- Mod01_aula2/main.cpp
+++ Mod01_aula2/main.cpp
@@ -7,7 +7,7 @@ int num1, num2;
 
     std::cout << "Digite dois numeros para somar: ";
     std::cin >> num1 >> num2;
-    std::cout << "Soma: " << num1 + num2 << std::endl;
+    std::cout << "Soma: " << (float)num1 / (float)num2 << std::endl;
     getchar();
     return 0;
 }
