5bd2c674f1866659eafac2ddff46604fb0f2d61e
gabrielrg <gabriel.rguerreiro@gmail.com>
2023-02-18 22:02:17 -0800

    modulo 02

---
+++ Mod01_aula2/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+
+float val;
+
+    std::cout << "Digite um valor: ";
+    std::cin >> val;
+    std::cout << "Valor digitado: " << val << std::endl;
+    getchar();
+    return 0;
+}
--- Proj01/main.cpp
+++ Proj01/main.cpp
@@ -3,5 +3,9 @@
 int main()
 {
     std::cout << "Hello world!";
+
+    std::cout << "Aula 1 do Modulo 1";
+
+    printf("Teste da funcao do C");
     return 0;
 }
