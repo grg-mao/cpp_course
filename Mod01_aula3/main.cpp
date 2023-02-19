e5c13cb777a8bf43a299df1cb3a47350ddc3c3a7
gabrielrg <gabriel.rguerreiro@gmail.com>
2023-02-18 22:37:24 -0800

    classe

---
+++ Mod01_aula3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <string> //classe de string no padrao c++
+
+int main()
+{
+    cout << "teste";
+    return 0;
+}
