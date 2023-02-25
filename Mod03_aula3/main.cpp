72ae3251f80b3c7cb80e5fd7b4ef7867bbfaba4c
gabrielrg <gabriel.rguerreiro@gmail.com>
2023-02-24 22:51:25 -0800

    tabuada

--- Mod03_aula3/main.cpp
+++ Mod03_aula3/main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include <iomanip>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::setw;
 
+void tabuada(unsigned num);
+
+
+int main()
+{
+  int num = 0;
+
+  cout << "Digite o valor: " << endl;
+  cin >> num;
+  tabuada(num);
+
+}
+
+void tabuada(unsigned num)
+{
+    int static i = 1;
+
+    cout << i << setw(2) << "x" << setw(2) << num << setw(2) << " = " << num*i << endl;
+    i++;
+
+    if (i > 10)
+    {
+        i = 1;
+        return;
+    }
+        tabuada(num);
+}
+
+
+
+
+/*
 unsigned long fact(unsigned long num);
+
 
 int main()
 {
@@ -25,3 +60,4 @@ unsigned long fact(unsigned long num)
     else
         return num*fact(num-1);
 }
+*/
