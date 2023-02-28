3c672dc14be10ce976bb9b8dc4131be67cbfad20
gabrielrg <gabriel.rguerreiro@gmail.com>
2023-02-27 23:10:29 -0800

    aritmetica de ponteiros

---
+++ Mod04_aula2/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int arr[6] = {4, 6, 2, 45, 3, 5};
+    int *ptr_arr, *ptr_arr2;
+
+    ptr_arr = &arr[0];
+    ptr_arr2 = arr;
+
+    cout << "sem *: " << ptr_arr << endl;   // endereco apontado
+    cout << "com *: " << *ptr_arr << endl;  // conteudo do endereco apontado
+    cout << "so arr: " << ptr_arr2 << endl;
+
+    cout << ptr_arr << endl;
+    ptr_arr += 2;
+    cout << ptr_arr << endl;
+
+    for(int i = 0; i<6; i++)
+        cout << &arr[i] << endl;
+
+    //cout << &arr[1] - &arr[0] << endl;
+    return 0;
+}
