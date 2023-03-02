4877f53bc126746101ad68c3d9248cef80f1bcc9
gabrielrg <gabriel.rguerreiro@gmail.com>
2023-03-01 22:14:13 -0800

    calculadora

--- Mod04_aula4/main.cpp
+++ Mod04_aula4/main.cpp
@@ -1,3 +1,53 @@
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int soma(int a, int b, char &sym);
+int subt(int a, int b, char &sym);
+int mult(int a, int b, char &sym);
+
+void calc(int(*operacao)(int, int, char &), int a, int b);
+
+// =========================================
+int main()
+{
+    calc(mult,5,8);
+
+    return 0;
+}
+// =========================================
+
+int soma(int a, int b, char &sym)
+{
+    sym = '+';
+    return a+b;
+}
+
+int subt(int a, int b, char &sym)
+{
+    sym = '-';
+    return a-b;
+}
+
+int mult(int a, int b, char &sym)
+{
+    sym = '*';
+    return a*b;
+}
+
+void calc(int(*operacao)(int, int, char &), int a, int b)
+{
+    char sym = '.'; // apenas inicializando
+    int res = (*operacao)(a, b, sym);
+
+    cout << a << sym << b << " = " << res << endl;
+}
+
+
+
+/* // Primeiro exemplo
 #include <iostream>
 
 using std::cout;
@@ -22,3 +72,4 @@ int soma10(int a)
 {
     return a+=10;
 }
+*/
